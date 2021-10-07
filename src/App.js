0b0b863243fb79cea2d4cff2d11472b90fe3d112
Matias D'Andrea <you@example.commatias31892@gmail.com>
2021-10-07 21:06:19 +0200

    implement code-splitting, add favicon, add term and conditions

--- src/App.js
+++ src/App.js
@@ -3,41 +3,45 @@ import './style/layout.css';
 import './style/form.css';
 import './style/buttons.css';
 import './style/utils.css';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/home/Home';
-import AddSite from './pages/add-site/AddSite';
-import Favorites from './pages/favorites/Favorites';
-import DailyMixPage from './pages/daily-mix/DailyMix.page';
 import Nav from './components/navbar/Nav';
 import { Toast } from './components/toast/Toast';
-import Admin from './pages/admin/Admin';
-import Login from './pages/login/Login';
 import ProtectedRoute from './components/ProtectedRoute';
+
+const Home = lazy(() => import('./pages/home/Home'));
+const Login = lazy(() => import('./pages/login/Login'));
+const Favorites = lazy(() => import('./pages/favorites/Favorites'));
+const DailyMix = lazy(() => import('./pages/daily-mix/DailyMix.page'));
+const AddSite = lazy(() => import('./pages/add-site/AddSite'));
+const Admin = lazy(() => import('./pages/admin/Admin'));
 
 function App() {
   return (
     <Router>
       <Nav />
-      <Switch>
-        <Route path='/daily-mix'>
-          <DailyMixPage />
-        </Route>
-        <Route path='/add-site'>
-          <AddSite />
-        </Route>
-        <Route path='/favorites'>
-          <Favorites />
-        </Route>
-        <ProtectedRoute path='/admin'>
-          <Admin />
-        </ProtectedRoute>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path='/daily-mix'>
+            <DailyMix />
+          </Route>
+          <Route path='/add-site'>
+            <AddSite />
+          </Route>
+          <Route path='/favorites'>
+            <Favorites />
+          </Route>
+          <ProtectedRoute path='/admin'>
+            <Admin />
+          </ProtectedRoute>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Suspense>
       <Toast />
     </Router>
   );
